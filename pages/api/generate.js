db773d3936ed4f47384c5ff9d50ad9fa92e46731
Mobey-eth <chantler.aob@gmail.com>
2023-05-09 01:08:23 +0100

    Initial commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,18 +9,19 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
     return;
   }
 
-  const animal = req.body.animal || '';
+  const animal = req.body.animal || "";
   if (animal.trim().length === 0) {
     res.status(400).json({
       error: {
         message: "Please enter a valid animal",
-      }
+      },
     });
     return;
   }
@@ -32,7 +33,7 @@ export default async function (req, res) {
       temperature: 0.6,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -41,22 +42,46 @@ export default async function (req, res) {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+function generatePrompt(userInput) {
+  // const capitalizedAnimal = animal[0].toUpperCase() + animal.slice(1).toLowerCase();
   return `Suggest three names for an animal that is a superhero.
 
 Animal: Cat
 Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
 Animal: Dog
 Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
+Animal: ${userInput}
 Names:`;
 }
+
+// Marv is a chatbot that reluctantly answers questions with sarcastic responses:
+
+// You: How many pounds are in a kilogram?
+// Marv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.
+// You: What does HTML stand for?
+// Marv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.
+// You: When did the first airplane fly?
+// Marv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.
+// You: What is the meaning of life?
+// Marv: I’m not sure. I’ll ask my friend Google.
+// You: What time is it?
+// Marv:
+
+// function generatePrompt(animal) {
+//   const capitalizedAnimal = animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+//   return `Suggest three names for an animal that is a superhero.
+
+// Animal: Cat
+// Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+// Animal: Dog
+// Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+// Animal: ${capitalizedAnimal}
+// Names:`;
+// }
